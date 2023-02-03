654f985451fbe3007b99a9a7a15607df45f5b2fb
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-02 19:33:24 -0600

    App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
 
 
 function App() {
@@ -8,6 +10,30 @@ function App() {
   //  Defininedo los STATE
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarPregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState({});
+  const [ creargasto, guardarCrearGasto ] = useState(false);
+
+
+  //  UseEFFECT - Actualiza el RESTANTE
+  useEffect(() => {
+    if(creargasto){
+      //  Agrega el nuevo presupuesto
+      guardarGastos([
+        ...gastos,
+        gasto
+      ]);
+
+      //  Resta del presupuesto actual
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+
+      //  Resetenado FALSE
+      guardarCrearGasto(false);
+    }
+  }, [gasto, creargasto, gastos, restante]);
+
 
 
 
@@ -17,23 +43,39 @@ function App() {
       <header><h1>Gasto Semanal</h1>
 
       <div className='contenido-principal contenido'>
-        <Pregunta 
-          guardarPresupuesto={guardarPresupuesto}
-          guardarRestante={guardarRestante}
-        />
-        
-        
-        <div className="row">
-          <div className='one-half column'>
-            <Formulario />
-          </div>
+        {/* Cargar condicional de componente - Si se cumple cierta condicion se ejecuta un Componente u el Otro Componnete  */}
+        {  mostrarPregunta ?  
+        (
+          <Pregunta 
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+            actualizarPregunta={actualizarPregunta}
+          />
+        )  : 
+        (
+          <div className="row">
+            <div className='one-half column'>
+              <Formulario 
+                guardarGasto={guardarGasto}
+                guardarCrearGasto={guardarCrearGasto}
+              />
+            </div>
 
-          <div className='one-half column'>
-            1
+            <div className='one-half column'>
+              <Listado 
+                gastos={gastos}
+              />
+
+              <ControlPresupuesto 
+                presupuesto={presupuesto}
+                restante={restante}
+              />
+            </div>
           </div>
-        </div>
+        )
+        }
+        
       </div>
-
 
       </header>
     </div>
