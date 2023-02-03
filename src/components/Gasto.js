cb4c8a7ebf38e71ad02a76b355d36da90422bdb3
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-02 19:30:50 -0600

    Gasto

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const Gasto = ( {gasto} ) => (
     <li className='gastos'>
         <p>
             {gasto.nombre}
-
             <span className='gasto'>$ {gasto.cantidad}</span>
         </p>
     </li>
 );
+
+
+/*  Documentando GASTO   */
+Gasto.propTypes = {
+    gasto: PropTypes.object.isRequired
+}
 
 export default Gasto;
