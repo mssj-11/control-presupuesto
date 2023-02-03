b3d2fd8bb2d553be10f1b7e095f7323ee98a17cc
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-02 19:29:12 -0600

    Error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,20 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 
 //  Componente ERROR,    Prop: Mensaje
 const Error = (  {mensaje}  ) => (
+
     <p className='alert alert-danger error'>
         {mensaje}
     </p>
+
 );
+
+
+/*  Documentando Error   */
+Error.propTypes = {
+    mensaje: PropTypes.string.isRequired
+}
 
 export default Error;
